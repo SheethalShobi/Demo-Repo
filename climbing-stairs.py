4a5037fb1a2ec1b0d83e854453e75512e3b7b921
Sheethal  Shobi <sheethalshobi@gmail.com>
2025-09-13 13:35:09 +0530

    Create climbing-stairs.py

---
+++ climbing-stairs.py
@@ -0,0 +1,23 @@
+Climbing Stairs
+You are given an integer n representing the number of steps to reach the top of a staircase. You can climb with either 1 or 2 steps at a time.
+
+Return the number of distinct ways to climb to the top of the staircase.
+
+Example 1:
+
+Input: n = 2
+
+Output: 2
+Explanation:
+
+1 + 1 = 2
+2 = 2
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one,two=1,1
+        for i in range(n-1):
+            temp=one
+            one=one+two
+            two=temp
+        return one   

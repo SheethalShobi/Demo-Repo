f8f59adefe38d05a2559a69f66d4730c517c3492
Sheethal  Shobi <sheethalshobi@gmail.com>
2025-09-17 16:02:45 +0530

    Create hello.java

---
+++ sample/hello.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 5, b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}

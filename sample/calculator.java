26fd09d17cb64e1c4f955307e2aad8e7754989c8
Sheethal  Shobi <sheethalshobi@gmail.com>
2025-09-17 16:01:11 +0530

    Create calculator.java

---
+++ sample/calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(5, 7);
+        System.out.println("Java Calculator Result: " + result);
+    }
+}
